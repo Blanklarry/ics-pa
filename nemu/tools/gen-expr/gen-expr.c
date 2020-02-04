7308c2be0fb90484b6724af6a163eb241229c493
LittleSec <jiexuan_he@smail.nju.edu.cn>
2020-02-04 14:37:18 +0800

    test: simply testing expr()

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -7,8 +7,38 @@
 
 // this should be enough
 static char buf[65536];
+static int cur_but_idx = 0;
+
+static inline void gen_num() {
+  int num = rand()&0xfff;
+  sprintf(buf+cur_but_idx, "%d", num);
+  cur_but_idx += strlen(buf+cur_but_idx);
+}
+
+static inline void gen(char c) {
+  buf[cur_but_idx++] = c;
+}
+
+static inline void gen_rand_op() {
+  switch (rand()%4) {
+    case 0: gen('+'); break;
+    case 1: gen('-'); break;
+    case 2: gen('*'); break;
+    case 3: gen('/'); break;
+  }
+}
+
+// todo:
+// 1. div 0
+// 2. buf overflow
+//*3. insert space randomly
+//*4. integer overflow
 static inline void gen_rand_expr() {
-  buf[0] = '\0';
+  switch (rand()%3) {
+    case 0: gen_num(); break;
+    case 1: gen('('); gen_rand_expr(); gen(')'); break;
+    default: gen_rand_expr(); gen_rand_op(); gen_rand_expr(); break;
+  }
 }
 
 static char code_buf[65536];
@@ -29,6 +59,8 @@ int main(int argc, char *argv[]) {
   }
   int i;
   for (i = 0; i < loop; i ++) {
+    cur_but_idx = 0;
+    memset(buf, 0, sizeof(buf));
     gen_rand_expr();
 
     sprintf(code_buf, code_format, buf);
