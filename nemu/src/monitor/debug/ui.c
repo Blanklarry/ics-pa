bd160cebfdc7c11a52dd611e6ed88e4402750b0c
LittleSec <jiexuan_he@smail.nju.edu.cn>
2020-02-04 14:36:52 +0800

    feat: infrastructure(simple debuger)

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -28,7 +28,7 @@ static char* rl_gets() {
 }
 
 static int cmd_c(char *args) {
-  cpu_exec(-1);
+  cpu_exec(-1); // hjx: -1(long)=0xffffffff=MAX(unsign long) !!!
   return 0;
 }
 
@@ -37,6 +37,12 @@ static int cmd_q(char *args) {
 }
 
 static int cmd_help(char *args);
+static int cmd_si_n(char *args);
+static int cmd_info_subcmd(char *args);
+static int cmd_p_expr(char *args);
+static int cmd_x_n_expr(char *args);
+static int cmd_w_expr(char *args);
+static int cmd_d_n(char *args);
 
 static struct {
   char *name;
@@ -46,6 +52,12 @@ static struct {
   { "help", "Display informations about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
+  { "si", "si [n] - Single step N instructions, N default 1", cmd_si_n },
+  { "info", "info SUBCMD - Print program states, SUBCMD: r(reg) or w(watchpoint)", cmd_info_subcmd },
+  { "p", "p EXPR - Calculate value of EXPR", cmd_p_expr },
+  { "x", "x N EXPR - Scand memory, take EXPR value as the starting memory address, and output N consecutive 4 bytes in hexadecimal form", cmd_x_n_expr },
+  { "w", "w EXPR - Set watchpoint, when value of EXPR has changed, stop the program", cmd_w_expr },
+  { "d", "d N - Delete N_st watchpoint", cmd_d_n },
 
   /* TODO: Add more commands */
 
@@ -76,11 +88,139 @@ static int cmd_help(char *args) {
   return 0;
 }
 
+static int cmd_si_n(char *args) {
+  uint64_t n = 1;
+  if (strtok(NULL, " ") != NULL) {
+    sscanf(args, "%lu", &n);
+    n = n == 0 ? 1 : n;
+  }
+  cpu_exec(n);
+  return 0;
+}
+
+static char cmd_info_subcmd_usage[] = "Usage: info SUBCMD, SUBCMD: r(reg) or w(watchpoint)";
+static int cmd_info_subcmd(char *args) {
+  /* extract the first argument */
+  char *arg = strtok(NULL, " ");
+
+  if (arg == NULL) {
+    /* no argument given */
+    printf("%s\n", cmd_info_subcmd_usage);
+    return 1;
+  }
+  else {
+    if (strcmp(arg, "r") == 0 || strcmp(arg, "reg") == 0) {
+      extern void isa_reg_display(void);
+      isa_reg_display();
+    }
+    else if (strcmp(arg, "w") == 0 || strcmp(arg, "watchpoint") == 0) {
+      display_wp();
+    }
+    else {
+      printf("Unknown command info '%s'\n%s\n", arg, cmd_info_subcmd_usage);
+      return 1;
+    }
+  }
+  return 0;
+}
+
+static char cmd_x_n_expr_usage[] = "Usage: x N EXPR, 0 < N < 0xffffffff";
+static int cmd_x_n_expr(char *args) {
+  uint32_t i, j, cnt, N;
+  char expression[MAX_EXPR_LEN];
+  int ret = sscanf(args, "%u %*[\"]%[^\"\n]", &N, expression);
+  // printf("hjx-debug: %d %s\n", N, expression);
+  if (ret != 2 || N < 1) {
+    printf("%s\n", cmd_x_n_expr_usage);
+    return 1;
+  }
+  bool expr_res = false;
+  vaddr_t start_vaddr = expr(expression, &expr_res);
+  Assert(expr_res, "Fail in Calculate value of EXPR");
+  for (i = 0, cnt = 0; i < (N>>3)+1; i++) { // N/8
+    printf("  0x%07x:   ", start_vaddr+cnt); // PMEM_SIZE: 7 hex num
+    for (j = 0; j < 8 && cnt < N; j++, cnt++) {
+      printf("0x%02x    ", vaddr_read(start_vaddr+cnt, 1));
+    }
+    printf("\n");
+  } 
+  return 0;
+}
+
+static int cmd_p_expr(char *args) {
+  char expression[MAX_EXPR_LEN];
+  sscanf(args, "%*[\"]%[^\"\n]", expression);
+  // sscanf(args, "%[^\n]", expression);
+  // printf("hjx-debug: %s\n", expression);
+  bool expr_res = false;
+  uint32_t expr_val = expr(expression, &expr_res);
+  Assert(expr_res, "Fail in Calculate value of EXPR");
+  printf("  Expression Value: ");
+  int i, exprlen = strlen(expression);
+  for (i = 0; i < exprlen; i++) {
+    if (expression[i] != ' ') {
+      printf("%c", expression[i]);
+    }
+  }
+  printf("=0x%x(%u)\n", expr_val, expr_val);
+  return 0;
+}
+
+static int cmd_w_expr(char *args) {
+  char expression[MAX_EXPR_LEN];
+  sscanf(args, "%*[\"]%[^\"\n]", expression);
+  WP *newwp = new_wp();
+  strncpy(newwp->wp_expr, expression, MAX_EXPR_LEN);
+  bool expr_res = false;
+  newwp->orig_expr_val = expr(expression, &expr_res);
+  Assert(expr_res, "Fail in Calculate value of EXPR");
+  printf("  Set watchpoint %-3d: %s(original val=%u)\n", newwp->NO, newwp->wp_expr, newwp->orig_expr_val);
+  return 0;
+}
+
+static int cmd_d_n(char *args) {
+  int n = -1;
+  sscanf(args, "%d", &n);
+  Assert(n >= 0 && n < NR_WP, "Error, Watchpoint num out of range [0, %d)", NR_WP);
+  WP dwp = { .NO=n };
+  free_wp(&dwp);
+  return 0;
+}
+
+
+// If test reg, ptr-deref... should modify code.
+void expr_test() {
+  // extern void isa_reg_display(void);
+  // isa_reg_display();
+  char line_buf[16+65536] = {0};
+  FILE *fp = fopen("/home/hjxjh/Desktop/ics2019/nemu/tools/gen-expr/input", "r");
+  assert(fp != NULL);
+
+  while (fgets(line_buf, 16+65536, fp) != NULL) {
+    char *result1 = strtok(line_buf, " ");
+    if (result1 == NULL) { continue; }
+    char *expression = result1 + strlen(result1) + 1;
+    expression[strlen(expression)-1] = '\0'; // line_buf end with \n
+    // printf("hjx-debug: now process: %s\n", expression);
+    bool succ;
+    uint32_t result2 = expr(expression, &succ);
+    if (succ) {
+      Assert(strtoul(result1, NULL, 10) == result2, "expr test error: %s = %s != %u", expression, result1, result2);      
+      // printf("hjx-debug: %s = %u\n", expression, result2);
+    }
+    
+  }
+  Log("Axpr_test pass.");
+  fclose(fp);  
+}
+
 void ui_mainloop(int is_batch_mode) {
   if (is_batch_mode) {
     cmd_c(NULL);
     return;
   }
+
+  expr_test();
 
   for (char *str; (str = rl_gets()) != NULL; ) {
     char *str_end = str + strlen(str);
