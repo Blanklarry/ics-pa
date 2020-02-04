ed0e11ea7975f6efa67d0cf35b3394878069be0a
LittleSec <jiexuan_he@smail.nju.edu.cn>
2020-02-04 14:29:52 +0800

    fix: x86 reg in CPU_state

--- nemu/src/isa/x86/include/isa/reg.h
+++ nemu/src/isa/x86/include/isa/reg.h
@@ -17,19 +17,25 @@ enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
  */
 
 typedef struct {
-  struct {
-    uint32_t _32;
-    uint16_t _16;
-    uint8_t _8[2];
-  } gpr[8];
+  union {
+    union {
+      uint32_t _32;
+      uint16_t _16;
+      uint8_t _8[2];
+    } gpr[8];
+
+    struct {
+      rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+    };    
+  };
 
   /* Do NOT change the order of the GPRs' definitions. */
 
   /* In NEMU, rtlreg_t is exactly uint32_t. This makes RTL instructions
    * in PA2 able to directly access these registers.
    */
-  rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
-
+  // rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+  
   vaddr_t pc;
 
 } CPU_state;
@@ -56,5 +62,7 @@ static inline const char* reg_name(int index, int width) {
     default: assert(0);
   }
 }
+
+uint32_t isa_reg_str2val(const char *s, bool *success);
 
 #endif
