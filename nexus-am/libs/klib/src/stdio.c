3c030ba28931fc99d56f431522df93d73fe2af86
LittleSec <jiexuan_he@smail.nju.edu.cn>
2020-02-12 21:12:16 +0800

    complete string and sprintf(d,s) lib in pa2.2

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -3,6 +3,23 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+char intstr[12];
+char *int2str(int d) {
+  int i = 12-1;
+  intstr[i--] = '\0';
+  if (d < 0) {
+    intstr[0] = '-';
+  }
+  while (d != 0) {
+    intstr[i--] = '0' + (d % 10);
+    d = d / 10;
+  }
+  if (intstr[0] == '-') {
+    intstr[i--] = '-';
+  }
+  return intstr+i+1;
+}
+
 int printf(const char *fmt, ...) {
   return 0;
 }
@@ -12,7 +29,43 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  return 0;
+  size_t n = strlen(fmt), i = 0, j = 0;
+  va_list ap;
+  va_start(ap, fmt);
+  char c, *s;
+  int d;
+  for (i = 0; i < n; i++) {
+    if (fmt[i] != '%') {
+      out[j++] = fmt[i];
+    }
+    else {
+      if (i+1 >= n) {
+        break;
+      }
+      switch(fmt[i+1]) {
+        case '%': out[j++] = '%'; break;
+        case 'd':
+          d = va_arg(ap, int);
+          strcpy(out+j, int2str(d));
+          j += strlen(int2str(d));
+          break;
+        case 's':
+          s = va_arg(ap, char*); 
+          strncpy(out+j, s, strlen(s));
+          j += strlen(s);
+          break;
+        case 'c':
+          c = va_arg(ap, int);
+          out[j++] = (char)c;
+          break;
+        default: assert(0);
+      }
+      i++;
+    }
+  }
+  va_end(ap);
+  out[j] = '\0';
+  return j;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -2,40 +2,171 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+// hjx-comment: when modify files in nexus-am/libs/klib/, you should re-compile in nexus-am, not in nemu.
+
 size_t strlen(const char *s) {
-  return 0;
+  if (s == NULL) {
+    return 0;
+  }
+  size_t ret = 0;
+  while (*s++ != '\0') {
+    ret++;
+  }
+  return ret;
 }
 
 char *strcpy(char* dst,const char* src) {
-  return NULL;
+  if (src == NULL || dst == NULL) {
+    return dst;
+  }
+  char *d = dst;
+  while (*src != '\0') {
+    *dst++ = *src++;
+  }
+  *dst = '\0';
+  return d;
 }
 
-char* strncpy(char* dst, const char* src, size_t n) {
-  return NULL;
+char* strncpy(char* dst, const char* src, size_t n) {  
+  size_t i = 0;
+  char *d = dst;
+  while (i < n && *src != '\0') {
+    *dst++ = *src++;
+    i++;
+  }
+  while (i < n) {
+    *dst++ = '\0';
+  }
+  return d;
 }
 
 char* strcat(char* dst, const char* src) {
-  return NULL;
+  if (src == NULL || dst == NULL) {
+    return dst;
+  }
+  char *d = dst;
+  while (*dst != '\0') { // if use *dst++, then should dst-- after this loop
+    dst++;
+  }
+  while (*src != '\0') {
+    *dst++ = *src++;
+  }
+  *dst = '\0';
+  return d;
 }
 
 int strcmp(const char* s1, const char* s2) {
-  return 0;
+  if (s1 == NULL && s2 == NULL) {
+    return 0;
+  }
+  if (s1 != NULL && s2 == NULL) {
+    return 1;
+  }
+  if (s1 == NULL && s2 != NULL) {
+    return -1;
+  }
+  while (*s1 != '\0' && *s2 != '\0' && *s1 == *s2) {
+    s1++;
+    s2++;
+  }
+  if (*s1 == *s2) {
+    return 0;
+  }
+  else if (*s1 > *s2) {
+    return 1;
+  }
+  else {
+    return -1;
+  }
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
+  assert(n > 0);
+  if (s1 == NULL && s2 == NULL) {
+    return 0;
+  }
+  if (s1 != NULL && s2 == NULL) {
+    return 1;
+  }
+  if (s1 == NULL && s2 != NULL) {
+    return -1;
+  }
+  size_t i = 0;
+  while (*s1 != '\0' && *s2 != '\0' && *s1 == *s2 && i < n) {
+    s1++;
+    s2++;
+    i++;
+  }
+  if (i == n) {
+    s1--;
+    s2--;
+  }
+  if (*s1 == *s2) {
+    return 0;
+  }
+  else if (*s1 > *s2) {
+    return 1;
+  }
+  else {
+    return -1;
+  }
   return 0;
 }
 
 void* memset(void* v,int c,size_t n) {
-  return NULL;
+  if (v == NULL) {
+    return v;
+  }
+  size_t i = 0;
+  char *v1 = (char*)v;
+  for (i = 0; i < n; i++) {
+    v1[i] = c;
+  }
+  return v;
 }
 
 void* memcpy(void* out, const void* in, size_t n) {
-  return NULL;
+  if (out == NULL || in == NULL) {
+    return out;
+  }
+  size_t i = 0;
+  char *out1 = (char*)out, *in1 = (char*)in;
+  for (i = 0; i < n; i++) {
+    out1[i] = in1[i];
+  }
+  return out;
 }
 
 int memcmp(const void* s1, const void* s2, size_t n){
-  return 0;
+  assert(n > 0);
+  if (s1 == NULL && s2 == NULL) {
+    return 0;
+  }
+  if (s1 != NULL && s2 == NULL) {
+    return 1;
+  }
+  if (s1 == NULL && s2 != NULL) {
+    return -1;
+  }
+  size_t i = 0;
+  unsigned char *s11 = (unsigned char*)s1, *s21 = (unsigned char*)s2;
+  for (i = 0; i < n; i++) {
+    if (s11[i] != s21[i]) {
+      break;
+    }
+  }
+  if (i == n) {
+    i--;
+  }
+  if (s11[i] > s21[i]) {
+    return 1;
+  }
+  else if (s11[i] < s21[i]) {
+    return -1;
+  }
+  else {
+    return 0;
+  }
 }
 
 #endif
