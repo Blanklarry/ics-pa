d2e291104f43039bb145ea8d0942041cdac1d217
LittleSec <jiexuan_he@smail.nju.edu.cn>
2020-02-18 15:10:45 +0800

    add: DEBUG macro for Log

--- nanos-lite/include/debug.h
+++ nanos-lite/include/debug.h
@@ -3,9 +3,15 @@
 
 #include "common.h"
 
+#define DEBUG
+
+#ifdef DEBUG
 #define Log(format, ...) \
   printk("\33[1;35m[%s,%d,%s] " format "\33[0m\n", \
       __FILE__, __LINE__, __func__, ## __VA_ARGS__)
+#else
+#define Log(format, ...) 
+#endif
 
 #define panic(format, ...) \
   do { \
